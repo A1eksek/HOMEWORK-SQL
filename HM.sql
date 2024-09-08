6420836dc0333a673ed0c6885639d5576a3e11b6
A1eksek <Alexey20080705@gmail.com>
2024-09-08 18:50:07 +0400

    Create HM.sql

---
+++ HM.sql
@@ -0,0 +1,30 @@
+-- SELECT ship_country
+-- FROM orders
+-- WHERE ship_country LIKE 'U%'
+
+-- SELECT order_id, customer_id, freight, ship_country
+-- FROM orders
+-- WHERE ship_country LIKE 'N%'
+-- ORDER BY freight DESC
+-- LIMIT 10
+
+-- SELECT last_name, first_name, home_phone, region
+-- FROM employees 
+-- WHERE region IS NULL
+
+-- SELECT COUNT(customer_id)
+-- FROM orders
+-- WHERE ship_region IS NOT NULL
+
+
+-- SELECT country, COUNT(*)
+-- FROM suppliers
+-- GROUP BY country
+-- ORDER BY COUNT(*) DESC
+
+SELECT ship_country, SUM(freight)
+FROM orders
+WHERE ship_region IS NOT NULL
+GROUP BY ship_country
+HAVING SUM(freight) > 2750
+ORDER BY SUM(freight) DESC
